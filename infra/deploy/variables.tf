fa06a577b40a0fbd840b29af26959ce4f4b3161d
mahesh ghagare <maheshghagare92@gmail.com>
2025-09-21 00:07:22 +0530

    9_20_10

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -18,9 +18,9 @@ variable "db_username" {
   default     = "recipeapp"
 }
 
-# variable "db_password" {
-#   description = "Password for the Terraform database"
-# }
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
 
 # variable "ecr_proxy_image" {
 #   description = "Path to the ECR repo with the proxy image"
