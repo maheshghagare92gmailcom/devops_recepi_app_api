c3ff7661fd2a3bd8a247ec95765b7bb402bd08e7
mahesh ghagare <maheshghagare92@gmail.com>
2025-09-27 15:36:52 +0530

    3 36 27

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -22,17 +22,17 @@ variable "db_password" {
   description = "Password for the Terraform database"
 }
 
-# variable "ecr_proxy_image" {
-#   description = "Path to the ECR repo with the proxy image"
-# }
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+}
 
-# variable "ecr_app_image" {
-#   description = "Path to the ECR repo with the API image"
-# }
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the API image"
+}
 
-# variable "django_secret_key" {
-#   description = "Secret key for Django"
-# }
+variable "django_secret_key" {
+  description = "Secret key for Django"
+}
 
 variable "dns_zone_name" {
   description = "Domain name"
